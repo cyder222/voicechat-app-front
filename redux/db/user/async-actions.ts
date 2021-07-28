9b673d0ce56ae307e1712f17c105b46cb3964380
taiki <boruboredo@gmail.com>
2021-07-29 08:35:09 +0900

    delete unused function sleep

--- redux/db/user/async-actions.ts
+++ redux/db/user/async-actions.ts
@@ -2,9 +2,6 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { getVoiceChatApi } from "../../../api-fetch/index";
 import { User } from "../../../codegen/api/fetch";
 
-const sleep = (microSecond: number) =>
-  {return new Promise((resolve) => {return setTimeout(resolve, microSecond);});};
-
 export interface fetchUserPayload{
   user: User;
 }
