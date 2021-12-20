ce1509f84bf061e3486eef268cbe1e54a1830fa8
taiki <boruboredo@gmail.com>
2021-12-21 06:50:19 +0900

    apiの呼び出しをssrとcsrで変えた

--- api-fetch/index.ts
+++ api-fetch/index.ts
@@ -1,7 +1,14 @@
+import { default as nodeFetch } from "node-fetch";
 import { Configuration, ConfigurationParameters, DefaultApi } from "../codegen/api/fetch";
 import { config } from "../config/constants";
 
 const configuration: ConfigurationParameters = { basePath: config.url.API_BASE_URL };
 export const getVoiceChatApi = (apiKey?: string): DefaultApi => {
-  return new DefaultApi(new Configuration({ ...configuration, apiKey: `Bearer ${apiKey}` }));
+  if(typeof window === "undefined")
+  {
+    return new DefaultApi(new Configuration({ ...configuration, apiKey: `Bearer ${apiKey}`, fetchApi: nodeFetch  }));
+  }else
+  {
+    return new DefaultApi(new Configuration({ ...configuration, apiKey: `Bearer ${apiKey}` }));
+  }
 };
