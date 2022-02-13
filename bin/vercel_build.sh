2818fd2484566d106191f5c8323d1944640437f3
taiki <boruboredo@gmail.com>
2022-02-13 16:06:49 +0900

    package更新

--- bin/vercel_build.sh
+++ bin/vercel_build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 git submodule update --init --recursive
-yarn
-yarn gen:typescript-fetch
-yarn add --dev typescript @types/node
+
+yarn add -D typescript @types/node
 yarn add -D babel-plugin-styled-components
+yarn gen:typescript-fetch
 yarn build
