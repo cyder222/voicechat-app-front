a1286a18541db2ae0362a7434de9109a586b7101
taiki <boruboredo@gmail.com>
2022-02-28 03:09:47 +0900

    nextconfigを修正

--- next.config.js
+++ next.config.js
@@ -1,11 +1,66 @@
-require("dotenv").config();                                                        
+/* eslint-env node */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 
-const path = require("path");                                                                                               
+/**
+ * @typedef {import("webpack").Configuration} WebpackConfig
+ * @typedef {import("webpack").Entry} WebpackEntry
+ * @typedef {import("webpack").EntryFunc} WebpackEntryFunc
+ * @typedef {{ webpack: (config: WebpackConfig) => WebpackConfig, [key: string]: any }} NextConfig
+ */
 
-module.exports = {
+require("dotenv").config();
+const CopyPlugin = require("copy-webpack-plugin");
+
+/** @type {(config: NextConfig) => NextConfig} */
+const nextConfig = {
     env: {
       // Reference a variable that was defined in the .env file and make it available at Build Time
       TEST_VAR: process.env.TEST_VAR,
       DEV_MODE: process.env.ENV_DEV_MODE,
     },
+    future: { webpack5: true },
+    webpack(config, { isServer, dev }) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        child_process: false,
+        net: false,
+        dns: false,
+        tls: false,
+      };
+      config.output.chunkFilename = isServer
+      ? `${dev ? "[name]" : "[name].[fullhash]"}.js`
+      : `static/chunks/${dev ? "[name]" : "[name].[fullhash]"}.js`;
+    // Overcome webpack referencing `window` in chunks
+    config.output.globalObject = "(typeof self !== 'undefined' ? self : this)";
+    config.plugins.push(
+      new CopyPlugin({
+          patterns: [
+              {
+                  from: "./node_modules/onnxruntime-web/dist/*.wasm",
+                  to: "[name].[ext]",
+              },
+              {
+                from: "./node_modules/onnxruntime-web/dist/*.wasm",
+                to: "static/[name].[ext]",
+              },
+              {
+                from: "./node_modules/onnxruntime-web/dist/*.wasm",
+                to: "server/[name].[ext]",
+              },
+              {
+                from: "./node_modules/onnxruntime-web/dist/*.wasm",
+                to: "static/chunks/[name].[ext]",
+              },
+              {
+                from: "./node_modules/onnxruntime-web/dist/*.wasm",
+                to: "./public/[name].[ext]",
+              },
+          ],
+      }),
+    );
+    return config;
+    },
   };           
+
+module.exports = nextConfig;
