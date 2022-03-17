f1f3df59b6b416c2c4a39b21b8b02617d65a56c5
taiki <boruboredo@gmail.com>
2022-03-18 01:17:21 +0900

    worklet用のgulpファイルを修正

--- bin/vercel_build.sh
+++ bin/vercel_build.sh
@@ -4,6 +4,7 @@ git submodule update --init --recursive
 yarn gen:typescript-fetch
 
 cd workers
+yarn
 yarn gulp
 cd ..
 yarn build
--- workers/gulpfile.js
+++ workers/gulpfile.js
@@ -1,9 +1,21 @@
-
+const del = require("del");
 const gulp = require("gulp");
 const babel = require("gulp-babel");
  
-gulp.task("default", function () {
-    return gulp.src(["./*.ts", "./**/*.ts", "!./node_modules/**"])
-        .pipe(babel())
-        .pipe(gulp.dest("../public/workers"));
+gulp.task("clear", function (cb) {
+    del([
+        "../public/workers/**/*",
+    ], cb);
 });
+
+gulp.task("build", function () {
+    return gulp.src(["./*.ts", "./**/*.ts", "!./node_modules/**"])
+        .pipe(babel());
+});
+
+gulp.task("copy", function () {
+    return gulp.src(["./*.js", "./**/*.js", "./**/*.onnx", "./**/*.json", "./**/*.wasm", "!./node_modules/**"])
+    .pipe(gulp.dest("../public/workers"));
+});
+
+gulp.task("default", gulp.series("build", "copy"));
