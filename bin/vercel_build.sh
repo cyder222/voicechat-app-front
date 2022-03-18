81456c42a613171aff3105c8b0db5cbb87e30ed5
taiki <boruboredo@gmail.com>
2022-03-18 11:58:43 +0900

    ビルドファイル修正

--- bin/vercel_build.sh
+++ bin/vercel_build.sh
@@ -6,7 +6,7 @@ yarn gen:typescript-fetch
 cd workers
 yarn install --ignore-engines
 
-yarn add -D gulp del gulp-babel --ignore-engines
+yarn add -D gulp del gulp-babel babel-plugin-add-import-extension babel-plugin-extension-resolver babel-plugin-replace-import-extension --ignore-engines
 
 yarn run build-worklet
 cd ..
