8aa934652b41fe82ab21f24bd8898a81c2594125
taiki <boruboredo@gmail.com>
2022-03-21 08:38:09 +0900

    コメントの削除と追加

--- workers/share/worker-config.ts
+++ workers/share/worker-config.ts
@@ -1,4 +1,4 @@
 export const WorkerConfig = {
-    kernelBufferSize: (16000 * 0.64) -1,
+    kernelBufferSize: (16000 * 0.64) -1, // 16000hz * 0.64秒(128/200) - 1は、 World側の出力が-1しないと129個になってしまったため
     RingBufferSize: 1024 * 32,
 };
